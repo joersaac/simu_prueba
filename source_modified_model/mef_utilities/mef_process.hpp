b09268ed422f73979cddb2508fbce48e35892469
joersaac <00159619@uca.edu.sv>
2023-06-30 14:41:02 -0600

    Modify: created changes on source_modifieed_model

--- source_modified_model/mef_utilities/mef_process.hpp
+++ source_modified_model/mef_utilities/mef_process.hpp
@@ -1,15 +1,3 @@
-float calculate_local_volume(
-    float x1, float y1, float z1, 
-    float x2, float y2, float z2, 
-    float x3, float y3, float z3, 
-    float x4, float y4, float z4
-) {
-    return abs(
-        ((x2 - x1) * (y3 - y1) * (z4 - z1) + (y2 - y1) * (z3 - z1) * (x4 - x1) + (z2 - z1) * (x3 - x1) * (y4 - y1)) - 
-        ((z2 - z1) * (y3 - y1) * (x4 - x1) + (y2 - y1) * (x3 - x1) * (z4 - z1) + (x2 - x1) * (z3 - z1) * (y4 - y1))) 
-        / 6;
-}
-
 float calculate_local_jacobian(
     float x1, float y1, float z1, 
     float x2, float y2, float z2, 
@@ -25,14 +13,17 @@ float calculate_local_jacobian(
 }
 
 void calculate_B(Matrix* B) {
+    //Fila 1
     B->set_value_on_matrix(-1, 0, 0);
     B->set_value_on_matrix(1, 0, 1);
     B->set_value_on_matrix(0, 0, 2);
     B->set_value_on_matrix(0, 0, 3);
+    //Fila 2
     B->set_value_on_matrix(-1, 1, 0);
     B->set_value_on_matrix(0, 1, 1);
     B->set_value_on_matrix(1, 1, 2);
     B->set_value_on_matrix(0, 1, 3);
+    //Fila 3
     B->set_value_on_matrix(-1, 2, 0);
     B->set_value_on_matrix(0, 2, 1);
     B->set_value_on_matrix(0, 2, 2);
@@ -46,12 +37,15 @@ void calculate_local_A (
     float x3, float y3, float z3,
     float x4, float y4, float z4)
     {
+        //Fila 1
         A->set_value_on_matrix( (y3 - y1)*(z4 - z1) - (y4 - y1)*(z3 - z1), 0, 0);  
         A->set_value_on_matrix(-(x3 - x1)*(z4 - z1) + (x4 - x1)*(z3 - z1), 0, 1);  
         A->set_value_on_matrix( (x3 - x1)*(y4 - y1) - (x4 - x1)*(y3 - y1), 0, 2);
+        //Fila 2
         A->set_value_on_matrix(-(y2 - y1)*(z4 - z1) + (y4 - y1)*(z2 - z1), 1, 0);  
         A->set_value_on_matrix( (x2 - x1)*(z4 - z1) - (x4 - x1)*(z2 - z1), 1, 1);  
         A->set_value_on_matrix(-(x2 - x1)*(y4 - y1) + (x4 - x1)*(y2 - y1), 1, 2);
+        //Fila 3
         A->set_value_on_matrix( (y2 - y1)*(z3 - z1) - (y3 - y1)*(z2 - z1), 2, 0);  
         A->set_value_on_matrix(-(x2 - x1)*(z3 - z1) + (x3 - x1)*(z2 - z1), 2, 1);  
         A->set_value_on_matrix( (x2 - x1)*(y3 - y1) - (x3 - x1)*(y2 - y1), 2, 2);
@@ -59,8 +53,6 @@ void calculate_local_A (
 
 void create_local_K(Matrix* K, int element_id, Mesh* M) {
     K->set_matrix_size(4, 4);
-
-    float k = M->get_problem_data(THERMAL_CONDUCTIVITY);
 
     float 
         x1 = M->get_element(element_id)->get_node_1()->get_x_coordinate(), 
@@ -80,7 +72,7 @@ void create_local_K(Matrix* K, int element_id, Mesh* M) {
         z4 = M->get_element(element_id)->get_node_4()->get_z_coordinate();
 
     float J = calculate_local_jacobian(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
-    float Volume = calculate_local_volume(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
+    float Volume = J/504;
 
     Matrix B(3, 4), A(3, 3);
 
@@ -103,15 +95,13 @@ void create_local_K(Matrix* K, int element_id, Mesh* M) {
     product_matrix_by_matrix(&A, &B, &res1);
     product_matrix_by_matrix(&At, &res1, &res2);
     product_matrix_by_matrix(&Bt, &res2, &res3);
-    product_scalar_by_matrix(k * Volume/(J * J), &res3, 4, 4, K);
+    product_scalar_by_matrix(Volume/(J * J), &res3, 4, 4, K);
 
     // std::cout << "\t\tLocal matrix created for Element " << element_id + 1 << ": "; K->show_matrix(); std::cout << "\n";
 }
 
 void create_local_b(Vector* b, int element_id, Mesh* M){
     b->set_vector_size(4);
-
-    float Q = M->get_problem_data(HEAT_SOURCE);
 
     float 
         x1 = M->get_element(element_id)->get_node_1()->get_x_coordinate(), 
@@ -132,10 +122,10 @@ void create_local_b(Vector* b, int element_id, Mesh* M){
 
     float J = calculate_local_jacobian(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
 
-    b->set_value_on_pos(Q * J/24, 0);
-    b->set_value_on_pos(Q * J/24, 1);
-    b->set_value_on_pos(Q * J/24, 2);
-    b->set_value_on_pos(Q * J/24, 3);
+    b->set_value_on_pos(J/252, 0);
+    b->set_value_on_pos(J/105, 1);
+    b->set_value_on_pos((19*J)/2520, 2);
+    b->set_value_on_pos(J/252, 3);
 
     // std::cout << "\t\tLocal vector created for Element " << element_id + 1 << ": "; b->show_vector(); std::cout << "\n";
 }
